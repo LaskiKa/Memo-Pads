8854c590fb0b766836e0b405870e8d69c508d696
Kamil Łaski <kamil.laski0@gmail.com>
2023-12-28 17:21:58 +0100

    project configuration

--- memo_pads_project/memo_pads_app/models.py
+++ memo_pads_project/memo_pads_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class MemoPads(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey("Category", on_delete=models.SET_NULL, null=True)
+    note = models.CharField()
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=64)
